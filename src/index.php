fcbb4b7ad024b2a7f1933ec8dccbdd2fb3f4062e
Yuuitiro Matsumoto <yuuitiro@vcnet.toyama.toyama.jp>
2018-01-30 08:54:39 +0800

    test update

--- src/index.php
+++ src/index.php
@@ -18,6 +18,7 @@
                 <h1>Simple PHP App</h1>
                 <h2>Congratulations</h2>
                 <p>Your PHP application is now running on a container in Amazon ECS.</p>
+                <p>update!</p>
                 <p>The container is running PHP version <?php echo phpversion(); ?>.</p>
                 <?php
                         $myfile = fopen("/var/www/my-vol/date", "r") or die("");
