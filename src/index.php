121687b022a5b0c9f1ba4d9ad23370d680c2a68b
Yuuitiro Matsumoto <yuuitiro@vcnet.toyama.toyama.jp>
2018-01-30 09:33:41 +0800

    update changes for feature 02 and more.

--- src/index.php
+++ src/index.php
@@ -19,7 +19,7 @@
                 <h2>Congratulations</h2>
                 <p>Your PHP application is now running on a container in Amazon ECS.</p>
                 <p>The container is running PHP version <?php echo phpversion(); ?>.</p>
-                <p>update!</p>
+                <p>update! update!</p>
                 <?php
                         $myfile = fopen("/var/www/my-vol/date", "r") or die("");
                         echo fread($myfile,filesize("/var/www/my-vol/date"));
